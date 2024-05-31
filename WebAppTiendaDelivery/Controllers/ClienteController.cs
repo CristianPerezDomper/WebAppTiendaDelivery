f4c586c790c40b74a799128b21ed943d1973425d
Cristian Perez <cristian.perez.domper@gmail.com>
2024-05-31 13:03:50 -0500

    Se creo la entidad cliente, el controlador para listar y la vista

---
+++ WebAppTiendaDelivery/Controllers/ClienteController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using WebAppTiendaDelivery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebAppTiendaDelivery.Controllers
+{
+    public class ClienteController : Controller
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ToString());
+
+        int numreg = 10;
+
+        // GET: Cliente
+        
+        List<Cliente> obtenerClientes()
+        {
+            //Crea listOf del tipo Cliente
+            List<Cliente> lstClientes = new List<Cliente>();
+
+            //Definimos un SqlCommand y su CommandType
+            SqlCommand cmd = new SqlCommand("usp_listaCliente",cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            //Abrimos conexión
+            cn.Open();
+
+            //Ejecutamos el SqlCommand
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            //Recuperamos los valores del SqlDataReader
+            while(dr.Read())
+            {
+                Cliente reg = new Cliente();
+
+                reg.CodigoCliente = dr["IdCliente"].ToString();
+                reg.NombreCliente = dr["NombreCia"].ToString();
+                reg.Direccion = dr["Direccion"].ToString();
+                reg.DescripcionPais = dr["NombrePais"].ToString();
+                reg.Telefono = dr["Telefono"].ToString();
+
+                lstClientes.Add(reg);
+            }
+
+            //Cerramos el SqlDataReader y la conexión a la BD
+            dr.Close();
+            cn.Close();
+
+            return lstClientes;
+        }
+
+
+
+        public ActionResult ListaClientes(int? pag = null)
+        {
+            int c = obtenerClientes().Count;
+            ViewBag.numreg = c %numreg != 0 ? c / numreg +1 : c / numreg;
+
+            int pageact = pag == null ? 0 : (int)pag;
+            int reginicio = pageact * numreg;
+            int regfin = reginicio + numreg;
+
+            List<Cliente> lista = new List<Cliente>();
+            for (int i = reginicio; i < regfin; i++)
+            {
+                if (i == c) break;
+                lista.Add(obtenerClientes()[i]);
+            }
+            return View(lista);
+        }
+    }
+}
---
+++ WebAppTiendaDelivery/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTiendaDelivery.Models
+{
+    public class Cliente
+    {
+        private string codigoCliente;
+        private string nombreCliente;
+        private string direccion;
+        private string descripcionPais;
+        private string telefono;
+
+        public string CodigoCliente { get => codigoCliente; set => codigoCliente = value; }
+        public string NombreCliente { get => nombreCliente; set => nombreCliente = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string DescripcionPais { get => descripcionPais; set => descripcionPais = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+    }
+}
